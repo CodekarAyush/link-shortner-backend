976e514660f7871b6b43b635098a922891fcb6a7
unknown <ayushGautam.dev@gmail.com>
2023-11-10 17:57:21 +0530

    db env

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const envVar = process.env
+const db = process.env.DATABASE
 exports.connectDB = ()=>{
-    mongoose.connect(`mongodb+srv://${envVar.DB_USERNAME}:${envVar.DB_PASSWORD}@linkshortner.hhnjazw.mongodb.net/${envVar.DB_NAME}?retryWrites=true&w=majority`).then(()=>{
+    mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
         console.log(`connected to the database`);
     }).catch((err)=>{
         console.log(err);
+        setTimeout(this.connectDB,5000)
     })
 }
